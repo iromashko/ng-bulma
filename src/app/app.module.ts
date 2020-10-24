acf1a1e24cbfd535ba97498cc82eddd04e4c520d
iromashko <iromashko@me.com>
2020-10-24 14:20:12 +0300

    part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreditCardFormComponent } from './credit-card-form/credit-card-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CreditCardFormComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CreditCardFormComponent, InputComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/credit-card-form/credit-card-form.component.ts
+++ src/app/credit-card-form/credit-card-form.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-credit-card-form',
   templateUrl: './credit-card-form.component.html',
-  styleUrls: ['./credit-card-form.component.scss']
+  styleUrls: ['./credit-card-form.component.scss'],
 })
 export class CreditCardFormComponent implements OnInit {
+  cardForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16),
+    ]),
+    cardNumber: new FormControl('', [Validators.required]),
+    expiration: new FormControl('', [Validators.required]),
+    securityCode: new FormControl('', [Validators.required]),
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log(this.cardForm);
   }
 
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    console.log(`form was submitted`);
+  }
 }
